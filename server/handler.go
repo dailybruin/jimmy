393484981144b915252ccb6bcf59dd63f6c93c3e
Calvin <calvin.chan.h@gmail.com>
2015-09-16 02:41:30 -0500

    Serving static files works

--- server/handler.go
+++ server/handler.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-    "net/http"
-    "html/template"
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
 )
 
 func getRouter() (*mux.Router, error) {
-    fs := http.FileServer(http.Dir("static"))
-    r := mux.NewRouter()
-    r.HandleFunc("/", indexHandler)
-    r.Handle("/static/{.+}", http.StripPrefix("/static/",fs))
-    // Alternatively, use PathPrefix. However, PathPrefix will show directory of files
-    // r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",fs))
-    return r, nil
+	fs := http.FileServer(http.Dir("static"))
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler)
+	r.Handle("/static/{(.+/?)*}", http.StripPrefix("/static/", fs))
+	// Alternatively, use PathPrefix. However, PathPrefix will show directory of files
+	//r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+	return r, nil
 }
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        /**
-         ** Check for valid path here
-        m := validPath.Find
-        if m == nil {
-            http.NotFound(w, r)
-            return
-        }
-        fn(w, r, m[2])
-        **/
-        fn(w, r)
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		/**
+		   ** Check for valid path here
+		  m := validPath.Find
+		  if m == nil {
+		      http.NotFound(w, r)
+		      return
+		  }
+		  fn(w, r, m[2])
+		  **/
+		fn(w, r)
+	}
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    t, _ := template.ParseFiles("templates/index.html")
-    t.Execute(w, nil)
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, nil)
 }
--- server/main.go
+++ server/main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-    "flag"
-    //"html/template"
-    "io/ioutil"
-    "log"
-    "net"
-    "net/http"
-    //"regexp"
+	"flag"
+	//"html/template"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	//"regexp"
 )
 
 var (
-    addr = flag.Bool("addr", false, "find open address and print to final-port.txt")
+	addr = flag.Bool("addr", false, "find open address and print to final-port.txt")
 )
 
 func main() {
-    flag.Parse()
-    r, err := getRouter()
-    if err != nil {
-        //handle error
-    }
-    http.Handle("/", r)
+	flag.Parse()
+	r, err := getRouter()
+	if err != nil {
+		//handle error
+	}
+	http.Handle("/", r)
 
-    if *addr {
-        l, err := net.Listen("tcp", "127.0.0.1:0")
-        if err != nil {
-            log.Fatal(err)
-        }
-        err = ioutil.WriteFile("final-port.txt", []byte(l.Addr().String()), 0644)
-        if err != nil {
-            log.Fatal(err)
-        }
-        s := &http.Server{}
-        s.Serve(l)
-        return
-    }
+	if *addr {
+		l, err := net.Listen("tcp", "127.0.0.1:0")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = ioutil.WriteFile("final-port.txt", []byte(l.Addr().String()), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		s := &http.Server{}
+		s.Serve(l)
+		return
+	}
 
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
